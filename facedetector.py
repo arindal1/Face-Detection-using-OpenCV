cace0f7ade2cd444d825b02d1d966c9333501f1b
Arindal Char <110285827+arindal1@users.noreply.github.com>
2023-08-16 11:24:43 +0530

    Add files via upload

---
+++ facedetector.py
@@ -0,0 +1,48 @@
+import cv2
+from simple_facerec import SimpleFacerec
+
+# Encode faces from a folder
+sfr = SimpleFacerec()
+sfr.load_encoding_images("InData/")
+
+# Load Camera
+cap = cv2.VideoCapture(2)
+
+
+while True:
+    ret, frame = cap.read()
+
+    # Detect Faces
+    face_locations, face_names = sfr.detect_known_faces(frame)
+    for face_loc, name in zip(face_locations, face_names):
+        y1, x2, y2, x1 = face_loc[0], face_loc[1], face_loc[2], face_loc[3]
+
+        cv2.putText(frame, name,(x1, y1 - 10), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 200), 2)
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 200), 4)
+
+    cv2.imshow("Frame", frame)
+
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
+
+# Accuracy calculation
+
+# total_frames = 0
+# correct_identifications = 0
+
+# while True:
+    # ... (rest of the loop)
+
+    # Count frames and correct identifications
+#     total_frames += 1
+#     if name == expected_name:  # Compare with actual name
+#         correct_identifications += 1
+
+# accuracy = (correct_identifications / total_frames) * 100
+# print(f"Accuracy: {accuracy:.2f}%")
+
